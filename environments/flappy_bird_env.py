d73795591ab81878a50be1cfcc4dd292507a519f
nick <nlinc1905@gmail.com>
2021-06-06 14:48:19 -0400

    flappy bird environment with tests

---
+++ environments/flappy_bird_env.py
@@ -0,0 +1,66 @@
+import numpy as np
+from ple import PLE
+from ple.games.flappybird import FlappyBird
+
+
+class FlappyBirdEnv:
+    def __init__(self):
+        """
+        Constructs an environment like the environments in OpenAI Gym's library.
+
+        There is only 1 possible action in Flappy Bird.  The getActionSet method retrieves all
+        possible actions.  Ordinarily a single action would be represented in a binary way, (0, 1),
+        where 1 is when the action is taken.  Here however, PLE returns [119, None], where 119 is the
+        action.
+        """
+        self.game = FlappyBird(pipe_gap=125)
+        self.env = PLE(self.game, fps=30, display_screen=False)
+        self.env.init()
+        #self.env.getGameState = self.game.getGameState  # maybe not necessary
+        self.action_map = self.env.getActionSet()  # [119, None]
+
+    def step(self, action):
+        """
+        Take an action and return the next observed state, reward, and done condition.
+
+        :param action: int - List index for self.action_map for the action to take.  This is
+            always either 0 or 1 for this particular game.
+        :return: next observed state (np array of shape (8,)), reward (float), done condition (bool)
+        """
+        action = self.action_map[action]  # retrieves the action from the key:value map
+        reward = self.env.act(action)     # calculates reward or fitness
+        done = self.env.game_over()       # checks if the game is over (Flappy Bird only ends when you lose)
+        obs = self.get_observation()
+        return obs, reward, done
+
+    def get_observation(self):
+        """
+        The game state returns a dictionary whose keys describe what each value represents.
+        This method returns the values of this dictionary as a numpy array, which
+        matches the convention from OpenAI's Gym library.
+
+        Game state returns a dict of 8 dimensions or features:
+        player_y, player_vel,
+        next_pipe_dist_to_player, next_pipe_top_y, next_pipe_bottom_y,
+        next_next_pipe_dist_to_player, next_next_pipe_top_y, next_next_pipe_bottom_y
+
+        :return: numpy array of shape (8,) representing dimensions or features of the game state
+        """
+        obs = self.env.getGameState()
+        return np.array(list(obs.values()))
+
+    def reset(self):
+        """
+        Resets the game's state and returns an observation from the reset state
+        """
+        self.env.reset_game()
+        return self.get_observation()
+
+    def set_display(self, boolean_value):
+        """
+        Changes the display condition, which determines whether or not to display the Pygame
+        in a screen to let you view what is going on.
+
+        :param boolean_value: either True or False
+        """
+        self.env.display_screen = boolean_value
---
+++ tests/test_flappy_bird_env.py
@@ -0,0 +1,42 @@
+import unittest
+
+from environments.flappy_bird_env import FlappyBirdEnv
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.fb = FlappyBirdEnv()
+
+    def test_init(self):
+        # Assert that display_screen is False by default and the action map equals
+        #   what is expected from the PLE documentation
+        assert not self.fb.env.display_screen
+        assert self.fb.action_map == [119, None]
+
+    def test_step(self):
+        # Assert that observation is returned with all 8 dimensions, the reward is a float
+        action = 0
+        obs, reward, done = self.fb.step(action=action)
+        assert len(obs) == 8
+        assert isinstance(reward, float)
+        assert isinstance(done, bool)
+
+    def test_get_observation(self):
+        # Assert that an observation with all 8 dimensions is returned
+        obs = self.fb.get_observation()
+        assert len(obs) == 8
+
+    def test_reset(self):
+        # Assert that resetting returns an observation and that the game is not over upon reset
+        obs = self.fb.reset()
+        assert len(obs) == 8
+        assert not self.fb.env.game_over()
+
+    def test_set_display(self):
+        # Assert that setting this to True updates the environment
+        self.fb.set_display(True)
+        assert self.fb.env.display_screen
+        # Assert that setting this to False updates the environment
+        self.fb.set_display(False)
+        assert not self.fb.env.display_screen
